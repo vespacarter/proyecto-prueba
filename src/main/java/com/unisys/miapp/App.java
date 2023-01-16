9258b0517f02fe6d1981d54d00c03c49aa61ecc5
Peralta Bordallo <Cesar.PeraltaBordallo@unisys.com>
2023-01-16 18:12:45 +0100

    Ejercicios basicos de variables

---
+++ src/main/java/com/unisys/miapp/App.java
@@ -0,0 +1,15 @@
+package com.unisys.miapp;
+
+public class App {
+    public static void main(String[] args) {
+        int numero = 35;
+        short marianico = 2;
+        long gasol = 1000000000;
+        boolean independencia = false;
+        double presupuesto = 7.9;
+        float longitud = 2.5f;
+        long suma = gasol + marianico;
+        System.out.println(suma);
+    }
+}
+
