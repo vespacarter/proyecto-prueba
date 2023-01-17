14b7d02e43e42061a6131e94d7d90003357a421e
vespacarter <skasez@gmail.com>
2023-01-17 18:27:58 +0100

    Ejercicios basicos de variables

--- src/main/java/com/unisys/miapp/App.java
+++ src/main/java/com/unisys/miapp/App.java
@@ -10,39 +10,39 @@ public class App {
         float longitud = 2.5f;
         long suma = gasol + marianico;
         System.out.println(suma);
-    //* Ejercicios Estructuras de control *//
+        //* Ejercicios Estructuras de control *//
 
         //* Ejercicio 4
 
         var i = 1;
-        while(i<=10){
+        while (i <= 10) {
             System.out.println("While Vuelta numero: " + i);
             i++;
         }
 
         //*Ejercicio 5
         i = 1;
-        do{
+        do {
             System.out.println("Do-While Vuelta numero: " + i);
             i++;
-        }while (i<=10);
+        } while (i <= 10);
 
         //* Ejercicio 6
 
-        for (i=0;i<=10;i++){
+        for (i = 0; i <= 10; i++) {
             System.out.println("For Vuelta numero: " + i);
         }
 
         //* ejercicio 7
         int edad = 21;
-        if(edad>=18){
+        if (edad >= 18) {
             System.out.println("Puede conducir un coche");
-        }else{
+        } else {
             System.out.println("NO puede conducir un coche");
         }
 
         //* Ejercicio 8
-        switch(edad){
+        switch (edad) {
             case 18:
                 System.out.println("Puede condudir un coche");
                 break;
@@ -53,6 +53,43 @@ public class App {
                 System.out.println("Su edad es: " + edad);
                 break;
         }
+        //* Ejercicio 9
+ /*       var i = 1;
+       label: while(i<=10){
+            System.out.println("While Vuelta numero: " + i);
+           i++;
+            if
+        }*/
+
+        //* Ejercicios de Arrays *//
+
+        //* Ejercicio 4
+        int[] miArray = new int[4];
+
+        //* Ejercicio 5
+
+        miArray[0] = 1;
+        miArray[1] = 2;
+        miArray[2] = 3;
+        miArray[3] = 4;
+
+        //* Ejercicio 6
+
+    //*    int i;
+        for (i = 0; i < miArray.length; i++) {
+
+            System.out.println(miArray[i]);
+        }
+
+        //* Ejercicio 7
+        i = 0;
+        do {
+            System.out.println("Do-While Valores miArray " + miArray[i]);
+            i++;
+        } while (i < miArray.length);
+
+        //* Ejercicio 8
+
     }
 }
 
