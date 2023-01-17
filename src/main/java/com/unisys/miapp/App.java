623c2213603f4e18fd52db06ada5af4105c5970a
vespacarter <skasez@gmail.com>
2023-01-17 16:12:10 +0100

    Ejercicios basicos de variables

--- src/main/java/com/unisys/miapp/App.java
+++ src/main/java/com/unisys/miapp/App.java
@@ -10,6 +10,49 @@ public class App {
         float longitud = 2.5f;
         long suma = gasol + marianico;
         System.out.println(suma);
+    //* Ejercicios Estructuras de control *//
+
+        //* Ejercicio 4
+
+        var i = 1;
+        while(i<=10){
+            System.out.println("While Vuelta numero: " + i);
+            i++;
+        }
+
+        //*Ejercicio 5
+        i = 1;
+        do{
+            System.out.println("Do-While Vuelta numero: " + i);
+            i++;
+        }while (i<=10);
+
+        //* Ejercicio 6
+
+        for (i=0;i<=10;i++){
+            System.out.println("For Vuelta numero: " + i);
+        }
+
+        //* ejercicio 7
+        int edad = 21;
+        if(edad>=18){
+            System.out.println("Puede conducir un coche");
+        }else{
+            System.out.println("NO puede conducir un coche");
+        }
+
+        //* Ejercicio 8
+        switch(edad){
+            case 18:
+                System.out.println("Puede condudir un coche");
+                break;
+            case 21:
+                System.out.println("Puede beber en USA");
+                // break;
+            default:
+                System.out.println("Su edad es: " + edad);
+                break;
+        }
     }
 }
 
