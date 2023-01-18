9490dfda4b4f3c44568b195b0ed2833be81edd39
vespacarter <skasez@gmail.com>
2023-01-18 18:30:18 +0100

    Ejercicios basicos de variables

---
+++ src/main/java/com/unisys/miapp/EjerciciosCadenas.java
@@ -0,0 +1,27 @@
+package com.unisys.miapp;
+
+public class EjerciciosCadenas {
+    public static void main(String[] args) {
+        //* Ejercicio 4
+        String saludo = "Hola";
+
+        //* Ejercicio 5
+        System.out.println("La longitud es de " + saludo.length() + " caracteres");
+
+        //* Ejercicio 6
+
+        System.out.println("El caracter en la posicion 4 es " + saludo.charAt(3));
+
+        //* Ejercicio 7
+        System.out.println("La cadena en mayusculas es " + saludo.toUpperCase());
+
+        //* Ejercicio 8
+        System.out.println("La cadena en minusculas es " + saludo.toLowerCase());
+
+        //* Ejercicio 9
+
+        System.out.println("EL caracter l esta en la posicion " + saludo.indexOf("l"));
+
+        // * Ejercicio 10
+    }
+}
---
+++ src/main/java/com/unisys/miapp/ProgramaClases.java
@@ -0,0 +1,7 @@
+package com.unisys.miapp;
+
+public class ProgramaClases {
+    public static void main(String[] args) {
+
+    }
+}
