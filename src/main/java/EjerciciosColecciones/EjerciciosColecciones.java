1ca91780b1863abf13f23557aaf92d9ca21ec3ac
vespacarter <skasez@gmail.com>
2023-01-25 18:10:01 +0100

    Ejercicios basicos de Colecciones

---
+++ src/main/java/EjerciciosColecciones/EjerciciosColecciones.java
@@ -0,0 +1,13 @@
+package EjerciciosColecciones;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class EjerciciosColecciones {
+    public static void main(String[] args) {
+        List<String> listado = new LinkedList<>();
+        listado.add("elemento01");
+        listado.add("listado02");
+        System.out.println(listado(0));
+    }
+}
